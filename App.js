ba73b61ee70800d4040deb952b15307533f6ccf8
Tudor <tudor@tudorg.ro>
2021-02-18 11:40:20 +0200

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ActivityIndicator } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+function LoadingIndicatorView() {
+  return <ActivityIndicator color='#009b88' size='large' />
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <WebView
+        originWhitelist={['*']}
+        source={{ uri: 'https://systemo.app/' }}
+        renderLoading={LoadingIndicatorView}
+        startInLoadingState={true}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +26,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
